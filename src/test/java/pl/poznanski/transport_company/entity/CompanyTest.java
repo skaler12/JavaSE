c3db6a8f1ba6eb75f6b942597134882087b168e0
skaler12 <gpoznanski98@gmail.com>
2020-08-18 01:27:44 +0200

    add new tests

--- src/test/java/pl/poznanski/transport_company/entity/CompanyTest.java
+++ src/test/java/pl/poznanski/transport_company/entity/CompanyTest.java
@@ -1,6 +1,10 @@
 package pl.poznanski.transport_company.entity;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +12,17 @@ class CompanyTest {
 
     @Test
     void addDriverList() {
+        Company company = new Company();
+        Driver driver = new Driver();
+        Assert.assertEquals(getExampleDriverList(),company.addDriverList(company,driver));
+    }
+
+    public static List<Driver> getExampleDriverList(){
+        List<Driver> driverList = new LinkedList<>(){};
+        Driver driver = new Driver();
+        driverList.add(driver);
+        Company company = new Company();
+        company.setDriverList(driverList);
+        return company.getDriverList();
     }
 }
