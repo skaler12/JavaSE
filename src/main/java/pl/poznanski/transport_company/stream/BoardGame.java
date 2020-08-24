3ceec25145232f99a28bb6a5d470b2f207507a92
skaler12 <gpoznanski98@gmail.com>
2020-08-24 19:39:40 +0200

    next commit - add stram

---
+++ src/main/java/pl/poznanski/transport_company/stream/BoardGame.java
@@ -0,0 +1,21 @@
+package pl.poznanski.transport_company.stream;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class BoardGame {
+    public final String name;
+    public final double rating;
+    public final BigDecimal price;
+    public final int minPlayers;
+    public final int maxPlayers;
+
+    public BoardGame(String name, double rating, BigDecimal price, int minPlayers, int maxPlayers) {
+        this.name = name;
+        this.rating = rating;
+        this.price = price;
+        this.minPlayers = minPlayers;
+        this.maxPlayers = maxPlayers;
+    }
+}
